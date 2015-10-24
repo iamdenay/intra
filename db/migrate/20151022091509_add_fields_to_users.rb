e268de3dccc1dec88f64889589042c4ce808c678
Atabay Ziyaden <iamdenay@gmail.com>
2015-10-24 23:48:35 +0600

    MNASnewKJ

--- db/migrate/20151022091509_add_fields_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddFieldsToUsers < ActiveRecord::Migration
-  def change
-    add_column :students, :first_name, :string
-    add_column :students, :last_name, :string
-    add_column :students, :student_id, :string
-  end
-end
